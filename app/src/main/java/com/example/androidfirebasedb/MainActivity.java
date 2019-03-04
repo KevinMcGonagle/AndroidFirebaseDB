4f8bedc7409a89bca2c9f7aff8c79191c8038e9d
Kevin <kevinmcgonagle97@gmail.com>
2019-03-04 20:06:37 +0000

    completed update and delete

--- app/src/main/java/com/example/androidfirebasedb/MainActivity.java
+++ app/src/main/java/com/example/androidfirebasedb/MainActivity.java
@@ -5,21 +5,28 @@ import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
 
     EditText edt_title,edt_content;
-    Button btn_post;
+    Button btn_post, btn_update, btn_delete;
     RecyclerView recyclerView;
 
     //firebase
@@ -28,6 +35,9 @@ public class MainActivity extends AppCompatActivity {
 
     FirebaseRecyclerOptions<Post> options;
     FirebaseRecyclerAdapter<Post,MyRecyclerViewHolder> adapter;
+
+    Post selectedPost;
+    String selectedKey;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,20 +48,84 @@ public class MainActivity extends AppCompatActivity {
         edt_content = (EditText)findViewById(R.id.edt_content);
         edt_title = (EditText)findViewById(R.id.edt_title);
         btn_post = (Button)findViewById(R.id.btn_post);
+        btn_update = (Button)findViewById(R.id.btn_update);
+        btn_delete = (Button)findViewById(R.id.btn_delete);
         recyclerView = (RecyclerView)findViewById(R.id.recycler_view);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
         firebaseDatabase = FirebaseDatabase.getInstance();
         databaseReference = firebaseDatabase.getReference("doorOpen");
 
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                    displayComment();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
         btn_post.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
+            public void onClick(View v) {
                 postComment();
             }
         });
 
+        btn_update.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                databaseReference
+                        .child(selectedKey)
+                        .setValue(new Post(edt_title.getText().toString(),edt_content.getText().toString()))
+                        .addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void aVoid) {
+                                Toast.makeText(MainActivity.this, "Updated", Toast.LENGTH_SHORT).show();
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(MainActivity.this, ""+e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+            }
+        });
+        btn_delete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                databaseReference
+                        .child(selectedKey)
+                        .removeValue()
+                        .addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void aVoid) {
+                                Toast.makeText(MainActivity.this, "Deleted", Toast.LENGTH_SHORT).show();
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(MainActivity.this, ""+e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+
+
+            }
+        });
+
         displayComment();
+    }
+
+    @Override
+    protected void onStop(){
+        if(adapter != null)
+            adapter.stopListening();
+        super.onStop();
     }
 
     private void postComment() {
@@ -78,9 +152,23 @@ public class MainActivity extends AppCompatActivity {
         adapter=
                 new FirebaseRecyclerAdapter<Post, MyRecyclerViewHolder>(options) {
                     @Override
-                    protected void onBindViewHolder(@NonNull MyRecyclerViewHolder holder, int position, @NonNull Post model) {
+                    protected void onBindViewHolder(@NonNull MyRecyclerViewHolder holder, final int position, @NonNull final Post model) {
                         holder.txt_title.setText(model.getTitle());
                         holder.txt_comment.setText(model.getContent());
+
+
+                        holder.setiItemClickListener(new IItemClickListener() {
+                            @Override
+                            public void onClick(View view, int postion) {
+                                selectedPost = model;
+                                selectedKey = getSnapshots().getSnapshot(position).getKey();
+                                Log.d("Key Item", ""+selectedKey);
+
+                                //bind data
+                                edt_content.setText(model.getContent());
+                                edt_title.setText(model.getTitle());
+                            }
+                        });
                     }
 
                     @NonNull
