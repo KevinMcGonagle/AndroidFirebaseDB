a9fd286957598c5a1118cf44937526717a8e0230
Kevin <kevinmcgonagle97@gmail.com>
2019-03-04 14:51:05 +0000

    fixed

--- app/src/main/java/com/example/androidfirebasedb/MainActivity.java
+++ app/src/main/java/com/example/androidfirebasedb/MainActivity.java
@@ -31,6 +31,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //FirebaseApp.initializeApp(this);
         setContentView(R.layout.activity_main);
 
         edt_content = (EditText)findViewById(R.id.edt_content);
